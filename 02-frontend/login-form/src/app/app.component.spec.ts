9afc64a023489630e899be4daff5af409b09a61a
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-12-07 16:17:43 +0100

    test: adjust unit test to match AppComponent\title

--- 02-frontend/login-form/src/app/app.component.spec.ts
+++ 02-frontend/login-form/src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'your-project-name' title`, () => {
+  it(`should have the 'login-form' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('your-project-name');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, your-project-name');
+    expect(app.title).toEqual('login-form');
   });
 });
